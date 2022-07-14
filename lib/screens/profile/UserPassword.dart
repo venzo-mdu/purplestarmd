994821fe380c5deaa5e1647e7dfa6c2e85f8f6ab
balagopinaath <balagopinaath@gmail.com>
2022-07-14 19:01:26 +0530

    without firebase

--- lib/screens/profile/UserPassword.dart
+++ lib/screens/profile/UserPassword.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:purplestarmd/screens/profile/SignIn.dart';
 import 'package:purplestarmd/widgets/CustomAppBar.dart';
 import 'package:purplestarmd/widgets/CustomBottomNavigation.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 import '../../constants.dart';
 import 'ProfilePage.dart';
@@ -20,8 +21,8 @@ class UserPassword extends StatefulWidget {
 
 class _UserPasswordState extends State<UserPassword> {
   final _formKey = GlobalKey<FormState>();
+  late String _email;
   String? password;
-  String? _email;
 
   @override
   void initState() {
@@ -32,7 +33,8 @@ class _UserPasswordState extends State<UserPassword> {
   // String emailAddress = _email;
 
   final _auth = FirebaseAuth.instance;
-  final TextEditingController emailAddress = TextEditingController();
+
+  // final TextEditingController emailAddress = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
 
   String? errorMessage;
@@ -91,17 +93,17 @@ class _UserPasswordState extends State<UserPassword> {
                       ),
                       decoration: InputDecoration(
                         contentPadding:
-                        EdgeInsets.symmetric(vertical: 5, horizontal: 20),
+                            EdgeInsets.symmetric(vertical: 5, horizontal: 20),
                         filled: true,
                         fillColor: mLightGrey,
                         enabledBorder: OutlineInputBorder(
                           borderSide:
-                          BorderSide(width: 1, color: Colors.transparent),
+                              BorderSide(width: 1, color: Colors.transparent),
                           borderRadius: BorderRadius.all(Radius.circular(30.0)),
                         ),
                         focusedBorder: OutlineInputBorder(
                           borderSide:
-                          BorderSide(width: 1, color: Colors.transparent),
+                              BorderSide(width: 1, color: Colors.transparent),
                           borderRadius: BorderRadius.all(Radius.circular(30.0)),
                         ),
                         hintText: '**********',
@@ -113,59 +115,63 @@ class _UserPasswordState extends State<UserPassword> {
                   padding: const EdgeInsets.symmetric(vertical: 25),
                   child: SizedBox(
                     height: 50,
-                    width: MediaQuery
-                        .of(context)
-                        .size
-                        .width,
+                    width: MediaQuery.of(context).size.width,
                     child: ElevatedButton(
                       onPressed: () async {
                         if (_formKey.currentState!.validate()) {
-                          try{
-                            await _auth.signInWithEmailAndPassword(email: _email, password: passwordController.text).then((uid) => {
-                              Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context)=> ProfilePage(user: user)))
-                            });
-                          } catch(e){
+                          try {
+                            // var user = _email;
+                            var user = _auth.currentUser;
+                            print(_email);
+                            print(user);
+                            await _auth
+                                .signInWithEmailAndPassword(
+                                    email: _email,
+                                    password: passwordController.text)
+                                .then((uid) => {
+                                      Navigator.of(context).pushReplacement(
+                                          MaterialPageRoute(
+                                              builder: (context) =>
+                                                  ProfilePage(user: user!)))
+                                    });
+                          } catch (e) {
                             print(e);
                           }
                         }
 
+                        // try{
+                        //   final user = await _auth.signInWithEmailAndPassword(
+                        //       email: _email, password: passwordController.text);
+                        //   if (user != null) {
+                        //     Navigator.pushAndRemoveUntil(MaterialPageRoute(
+                        //         builder: (context) => ProfilePage(user: user)),
+                        //         ModalRoute.withName('/');
+                        //     );
+                        //   }
+                        // } catch(e) {
+                        //   print(e);
+                        // }
 
-
-
-                          // try{
-                          //   final user = await _auth.signInWithEmailAndPassword(
-                          //       email: _email, password: passwordController.text);
-                          //   if (user != null) {
-                          //     Navigator.pushAndRemoveUntil(MaterialPageRoute(
-                          //         builder: (context) => ProfilePage(user: user)),
-                          //         ModalRoute.withName('/');
-                          //     );
-                          //   }
-                          // } catch(e) {
-                          //   print(e);
-                          // }
-
-                          // signIn(_email!, passwordController.text);
-                          // if(_formKey.currentState!.validate()) {
-                          //   Navigator.push(context, MaterialPageRoute(builder: (context) => ProfilePage(user: null,)));
-                          // }
-                        },
-                        style: ElevatedButton.styleFrom(
+                        // signIn(_email!, passwordController.text);
+                        // if(_formKey.currentState!.validate()) {
+                        //   Navigator.push(context, MaterialPageRoute(builder: (context) => ProfilePage(user: null,)));
+                        // }
+                      },
+                      style: ElevatedButton.styleFrom(
                         shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(20),
+                          borderRadius: BorderRadius.circular(20),
                         ),
                         primary: mPrimaryColor,
                         padding:
-                        EdgeInsets.symmetric(vertical: 13, horizontal: 130),
+                            EdgeInsets.symmetric(vertical: 13, horizontal: 130),
                         textStyle: TextStyle(
-                        fontSize: 14,
-                        fontWeight: FontWeight.bold,
+                          fontSize: 14,
+                          fontWeight: FontWeight.bold,
                         ),
-                        ),
-                        child: Text(
+                      ),
+                      child: Text(
                         'SUBMIT',
-                        style: TextStyle
-                        }(fontFamily: 'BebasNeue', fontSize: 20),
+                        style: TextStyle(fontFamily: 'BebasNeue', fontSize: 20),
                         // textScaleFactor: 1.5,
                       ),
                     ),
@@ -180,4 +186,3 @@ class _UserPasswordState extends State<UserPassword> {
     );
   }
 }
-
